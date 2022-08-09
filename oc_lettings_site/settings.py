95e9e607d5ff065fd7b16ca9186ca5f9a4198355
dev@KC <dev@kitconseil.fr>
2022-08-09 10:29:37 +0200

    feat: set allowedhost

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost",  '.herokuapp.com']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost",  '.herokuapp.com', 'young-reaches-84360.herokuapp.com']
 
 
 # Application definition
